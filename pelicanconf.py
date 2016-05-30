995a2384ba1e7f764015ec86470f2e755b203833
whopper-sculder <wjhopper510@gmail.com>
2016-05-29 21:14:37 -0400

    switched to new custom theme academida-red

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,16 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Will Hopper'
 SITENAME = 'Will Hopper'
+SITESUBTITLE = 'Cognitive Psychology, Modeling, and Coding'
 SITEURL = ''
 
 PATH = 'content'
-TIMEZONE = 'America/New_York'
+STATIC_PATHS = ['img','pres','pub']
 
+TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
 DEFAULT_DATE = 'fs'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -18,17 +21,15 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = 'academia-red'
+AVATAR = 'will-web.jpg'
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
